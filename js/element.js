1d15cdc81d90b8ad01a76d2af50df4a0f63a41f0
supin <qorgkr26@sookmyung.ac.kr>
2021-02-12 02:09:25 +0900

    del element.js

--- js/element.js
+++
@@ -1,7 +0,0 @@
-function setElement() {
-  const bookArray = document.querySelector('.bookList');
-  const listBox = document.createElement('ul');
-  bookArray.appendChild(listBox);
-}
-
-export { setElement };
